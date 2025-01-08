b7e88a7da8ca88d315e09f82b6f45191e7ecb8e7
UriShiber <urishiber@gmail.com>
2025-01-08 18:33:19 +0200

    add the router to server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,13 @@ dotenv.config();
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import express, { Express } from "express";
+import { commentsRouter } from "./comments/comments_route";
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/comments', commentsRouter);
+
 
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
